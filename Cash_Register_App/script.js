e630279752464ed784bccb32dce12aaf8bdd2871
shubhaaaaaaa <shubhakhadgi007@gmail.com>
2024-11-13 22:10:13 +0545

    add cash register app

---
+++ Cash_Register_App/script.js
@@ -0,0 +1,34 @@
+const shoppingCart = []
+
+//list of products available for user to buy
+const availableListings = [
+    { product: 'Phone', price: 300 },
+    { product: 'Smart TV', price: 220 },
+    { product: 'Gaming Console', price: 150 },
+]
+
+//display the available products in the shopping list section
+let cashRegisterList = ''
+availableListings.forEach(element => {
+    cashRegisterList += `<p>Product: ${element.product}, Price: ${element.price}</p>`
+});
+document.querySelector('.cash-register-products').innerHTML = cashRegisterList;
+
+//addItem button functionality
+const addItem = () => {
+    let userPrompt = (prompt('What item do you want to buy?')).toLowerCase()
+    availableListings.forEach(element => {
+        if (userPrompt == (element.product).toLowerCase()) {
+            shoppingCart.push(element)
+            document.querySelector('.div--shopping-cart').innerHTML += `<p>Product: ${element.product}, Price: ${element.price}</p>`; 
+        }
+    });
+}
+
+//calculateTotalPrice button functionality
+const calculateTotalPrice = () => {
+    let totalPrice = shoppingCart.reduce((total, item) => {
+        return total + item.price
+    }, 0)
+    document.querySelector('.div--shopping-cart').innerHTML += `<p><b>Total: </b>${totalPrice}</p>`;     
+}
