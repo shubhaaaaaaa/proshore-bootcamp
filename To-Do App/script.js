dba4634949140641d0a0c5e416f609a1679cc593
shubhaaaaaaa <shubhakhadgi007@gmail.com>
2024-11-20 12:07:23 +0545

    :sparkles: add todo with localStorage

--- To-Do App/script.js
+++ To-Do App/script.js
@@ -4,12 +4,15 @@ const modal = document.getElementById('js-modal');
 
 let items = []
 
+//add item to table
 function addItem(newTodo) {
+    //tr element
     const element_tr = document.createElement('tr')
     element_tr.classList.add('border-b', 'border-neutral-200')
 
+    //td element - checkbox col
     const element_complete = document.createElement('td')
-    element_complete.classList.add('text-center')
+    element_complete.classList.add('text-center', 'px-5')
     const checkbox = document.createElement('input')
     checkbox.type = 'checkbox'
     checkbox.addEventListener('click', () => {
@@ -17,10 +20,12 @@ function addItem(newTodo) {
     })
     element_complete.appendChild(checkbox)
     
+    //td element - list todo col
     const element_task = document.createElement('td')
     element_task.textContent = newTodo
-    element_task.classList.add('border-s','border-e', 'border-neutral-200','px-6', 'py-4', 'break-words', 'whitespace-normal')
+    element_task.classList.add('border-s','border-e', 'border-neutral-200','px-6', 'py-4', 'break-words', 'whitespace-normal', 'w-3/4')
 
+    //td element - trash icon col
     const element_more = document.createElement('td')
     const trashIcon = document.createElement('i')
     trashIcon.classList.add('fas', 'fa-trash-alt', 'cursor-pointer', 'px-5')
@@ -29,27 +34,37 @@ function addItem(newTodo) {
         removeFromLocal(newTodo)
     }
     element_more.appendChild(trashIcon)
+
+    //appending all td inside tr 
     element_tr.appendChild(element_complete)
     element_tr.appendChild(element_task)
     element_tr.appendChild(element_more)
 
+    //apend the tr to main table
     toDoList.appendChild(element_tr)
 }
 
 //Popup Modal
-window.displayModal = () => {
+const toggleModal = () => {
     modal.classList.toggle('hidden');
     modal.classList.toggle('block');
 };
 
+//focus on input as soon as modal opens
 window.onclick = function (event) {
     if (event.target == modal) {
-        modal.classList.add('hidden');
-        modal.classList.remove('block');
+        toggleModal()
     } else if (modal.classList.contains('block')) {
         userInput.focus();
     }
 };
+
+//open modal when 'n' is pressed
+document.addEventListener('keypress', (event) => {
+    if (event.key == 'n' || event.key == 'N') {
+        toggleModal()
+    }
+})
 
 //Modal - Add button functionality
 const addButtonModal = () => {
@@ -59,8 +74,7 @@ const addButtonModal = () => {
     storeLocal(inputValue)
     userInput.value = ''
 
-    modal.classList.add('hidden');
-    modal.classList.remove('block');
+    toggleModal()
 }
 
 //Pressing enter to add new item
@@ -70,8 +84,7 @@ userInput.addEventListener('keypress', function (event) {
         storeLocal(this.value)
         this.value = ''
         
-        modal.classList.add('hidden');
-        modal.classList.remove('block');
+        toggleModal()
     }
 })
 
@@ -94,6 +107,7 @@ const storeLocal = (item) => {
     items.push(item)
     localStorage.setItem('storeLocal', JSON.stringify(items))
 }
+getLocal()
 
 //Remove from local
 const removeFromLocal = (item) => {
@@ -101,4 +115,7 @@ const removeFromLocal = (item) => {
     localStorage.setItem('storeLocal', JSON.stringify(items))
 }
 
-getLocal()
+//Get date
+document.getElementById('date').innerText = new Date().toLocaleDateString('en-us', { weekday:"short", day:"numeric", year:"numeric", month:"long"}) 
+
+
